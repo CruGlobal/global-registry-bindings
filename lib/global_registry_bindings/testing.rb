8cef9b1eb84c45d913a4ba63d218922f2ff632e4
Brian Zoetewey <brian.zoetewey@ccci.org>
2017-10-16 10:48:45 -0400

    Add a testing help to disable adding sidekiq workers during tests.

---
+++ lib/global_registry_bindings/testing.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+module GlobalRegistry #:nodoc:
+  module Bindings #:nodoc:
+    class Testing
+      class << self
+        attr_accessor :__test_mode
+
+        def __set_test_mode(mode)
+          if block_given?
+            current_mode = __test_mode
+            begin
+              self.__test_mode = mode
+              yield
+            ensure
+              self.__test_mode = current_mode
+            end
+          else
+            self.__test_mode = mode
+          end
+        end
+
+        def skip_workers!(&block)
+          __set_test_mode(:skip, &block)
+        end
+
+        def disable!(&block)
+          __set_test_mode(:disable, &block)
+        end
+
+        def enabled?
+          __test_mode != :disable
+        end
+
+        def disabled?
+          __test_mode == :disable
+        end
+
+        def skip?
+          __test_mode == :skip
+        end
+      end
+    end
+
+    class Worker
+      class << self
+        alias perform_async_real perform_async
+
+        def perform_async(*args)
+          return if GlobalRegistry::Bindings::Testing.skip?
+          perform_async_real(*args)
+        end
+      end
+    end
+  end
+end
+
+# Default to disabling testing helpers
+GlobalRegistry::Bindings::Testing.disable!
